86d578263793b2a2c378b0d6b4f7431a791285d4
ahmetcankaratas <karatascanahmet@yahoo.com>
2022-12-20 11:08:38 +0300

    add recipes list

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class ingredients{
+    constructor(public name: string, public amount: number){
+  }
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ingredients } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: ingredients[] = [
+    new ingredients('Apples', 5),
+    new ingredients('Tomatoes', 10)
+    
+  ];
   constructor() { }
 
   ngOnInit(): void {
