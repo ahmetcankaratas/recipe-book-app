bf87567abbe4a43115fd3fb93eadff1b86b33f92
ahmetcankaratas <karatascanahmet@yahoo.com>
2023-01-03 14:47:33 +0300

    add firebase storage for saving recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -27,6 +27,7 @@ import { RecipesModule } from './recipes/recipes.module';
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    HttpClientModule,
     RecipesModule
   ],
   providers: [ShoppingListService, RecipeService],
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -5,55 +5,55 @@ import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
-
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
-  recipesChanged = new Subject<Recipe[]>()
+  private recipes: Recipe[] = [
+    new Recipe(
+      "Tasty Schnitzel",
+      "A super-tasty Schnitzel - just awesome!",
+      "https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG",
+      [new ingredient("Meat", 1), new ingredient("French Fries", 20)]
+    ),
+    new Recipe(
+      "Big Fat Burger",
+      "What else you need to say?",
+      "https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg",
+      [new ingredient("Buns", 2), new ingredient("Meat", 1)]
+    ),
+  ];
 
-    private recipes: Recipe[] = [
-        new Recipe(
-          'Tasty Schnitzel',
-          'A super-tasty Schnitzel - just awesome!',
-          'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
-          [
-            new ingredient('Meat', 1),
-            new ingredient('French Fries', 20)
-          ]),
-        new Recipe('Big Fat Burger',
-          'What else you need to say?',
-          'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
-          [
-            new ingredient('Buns', 2),
-            new ingredient('Meat', 1)
-          ])
-      ];
+  constructor(private shoppingListService: ShoppingListService) {}
 
-    constructor(private shoppingListService: ShoppingListService){}
-    
-    getRecipes(){
-        return this.recipes.slice(); // slice() returns a copy of the array
-    }
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
-    getRecipe(index: number){
-      return this.recipes[index];
-    }
+  getRecipes() {
+    return this.recipes.slice(); // slice() returns a copy of the array
+  }
 
-    addIngredientsToShoppingList(ingredients: ingredient[]){
-      this.shoppingListService.addIngredients(ingredients);
-    }
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
 
-    addRecipe(recipe: Recipe){
-      this.recipes.push(recipe);
-      this.recipesChanged.next(this.recipes.slice());
-    }
+  addIngredientsToShoppingList(ingredients: ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
 
-    updateRecipe(index: number, newRecipe: Recipe){
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
     this.recipes[index] = newRecipe;
     this.recipesChanged.next(this.recipes.slice());
-    }
+  }
 
-    deleteRecipe(index: number){
-      this.recipes.splice(index,1);
-      this.recipesChanged.next(this.recipes.slice());
-    }
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
 }
